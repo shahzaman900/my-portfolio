7fdc1d9bd74fb486ef6e2307ff734e6791a41650
shahzaman900 <shahzamanlive2@gmail.com>
2024-02-02 01:00:47 +0500

    implement mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,20 @@
-import { Menu } from 'lucide-react';
+import { MenuIcon, XCircle } from 'lucide-react';
 import profile from '../assets/profile.svg'
 import { ModeToggle } from './mode-toggle';
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
 import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet';
 
 
 function Header() {
@@ -39,18 +44,57 @@ function Header() {
         <div className='flex gap-4 items-center justify-center'>
           <ModeToggle></ModeToggle>
           <div className='md:hidden'>
+            {/* <Drawer direction='right'>
+              <DrawerTrigger><Button variant={'outline'}><MenuIcon /></Button></DrawerTrigger>
+              <DrawerContent>
+                <DrawerClose><XCircle /></DrawerClose>
+                <div>
+                  <nav>
+                    <DrawerClose>
+                      <a href="#">dfsd</a>
+                    </DrawerClose>
+                  </nav>
+                </div>
+              </DrawerContent>
+            </Drawer> */}
             <Sheet>
-              <SheetTrigger><Menu /></SheetTrigger>
+              <SheetTrigger><Button variant={'outline'}><MenuIcon /></Button></SheetTrigger>
               <SheetContent>
                 <SheetHeader>
-                  <SheetTitle>Are you absolutely sure?</SheetTitle>
                   <SheetDescription>
-                    This action cannot be undone. This will permanently delete your account
-                    and remove your data from our servers.
+                    <div className='flex flex-col gap-4 pt-8'>
+                      <SheetClose asChild>
+                        <Link to={"/"} >
+                          <div className='p-4 hover:bg-accent rounded-md'><span>01.</span> Home </div>
+                        </Link>
+
+                      </SheetClose>
+                      <SheetClose asChild>
+                          <Link to={"about"} >
+                            <div className='p-4 hover:bg-accent rounded-md'><span>02.</span> About</div>
+                          </Link>
+                      </SheetClose>
+                      <SheetClose asChild>
+                        <Link to={"projects"} >
+                          <div className='p-4 hover:bg-accent rounded-md'><span>03.</span> Projects</div>
+                        </Link>
+                      </SheetClose>
+                      <SheetClose asChild>
+                        <Link to={"experience"} >
+                          <div className='p-4 hover:bg-accent rounded-md'><span>04.</span> Experience</div>
+                        </Link>
+                      </SheetClose>
+                      <SheetClose asChild>
+                        <Link to={"contact"} >
+                          <div className='p-4 hover:bg-accent rounded-md'><span>05.</span> Contact</div>
+                        </Link>
+                      </SheetClose>
+                    </div>
                   </SheetDescription>
                 </SheetHeader>
               </SheetContent>
             </Sheet>
+
           </div>
         </div>
       </div>
