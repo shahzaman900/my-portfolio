e2619433721e8d882da7c2721668a6f705e18d6d
shahzaman900 <shahzamanlive2@gmail.com>
2024-02-02 22:26:15 +0500

    update intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,7 +4,7 @@ import { TypeAnimation } from 'react-type-animation';
 function Introduction() {
   return (
     <div className=' flex gap-8 flex-col md:w-3/5 p-4 pb-14'>
-      <h6 className='md:text-xl sm:text-lg text-lg text-foreground/80 pr-2'>Hi, my name is <span className='px-3 py-1 bg-primary rounded text-white'>Shah Zaman</span></h6>
+      <h6 className='md:text-xl sm:text-lg text-lg text-foreground/80 pr-2'>Hi, my name is <span className='px-3 py-1  rounded text-primary border-b-2 border-primary italic'>Shah Zaman</span></h6>
       <TypeAnimation
         sequence={[
           // Same substring at the start will only be typed out once, initially
@@ -19,8 +19,8 @@ function Introduction() {
         className=''
         repeat={Infinity}
       />
-      <h6 className='text-xl text-foreground/80 '><span className='text-primary pr-2'>----</span>Lead frontend team at <span className='text-white font-bold'>Cognitive Healthcare International</span></h6>
-      <h6 className='text-xl text-foreground/80 '><span className='text-primary pr-2'>----</span>Checkout my <Link to={'projects'} ><span className="text-primary underline cursor-pointer after:content-['_↗']">Projects</span></Link></h6>
+      <h6 className='text-xl text-foreground/80 '><span className='text-primary pr-2'>--</span>Lead frontend team at <span className='text-white font-bold'>Cognitive Healthcare International</span></h6>
+      <h6 className='text-xl text-foreground/80'><span className='text-primary pr-2'>--</span>Checkout my <Link to={'projects'} ><span className="text-primary underline cursor-pointer after:content-['_↗']">Projects</span></Link></h6>
     </div>
   )
 }
