84524af5ccf23e1d40b0521effd76c7a152f07ca
shahzaman900 <shahzamanlive2@gmail.com>
2024-02-03 11:11:36 +0500

    add resume on front page

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { TypeAnimation } from 'react-type-animation';
+import { Button } from './ui/button';
 
 function Introduction() {
   return (
@@ -21,6 +22,11 @@ function Introduction() {
       />
       <h6 className='text-xl text-foreground/80 '><span className='text-primary pr-2'>*</span>Lead frontend team at <span className='text-white font-bold'>Cognitive Healthcare International</span></h6>
       <h6 className='text-xl text-foreground/80'><span className='text-primary pr-2'>*</span>Checkout my <Link to={'projects'} ><span className="text-primary underline cursor-pointer after:content-['_↗']">Projects</span></Link></h6>
+      <div>
+        <a href="https://drive.google.com/file/d/1xCdFOLMx0uReY1MhsOq3mH7Yf81Rqace/view?usp=sharing" target="_blank">
+          <Button variant={'outline'}>Get My Resume</Button>
+        </a>
+      </div>
     </div>
   )
 }
